23a99a456e33a945813d6e337cffafcba8f2c60d
S.Percentage <Syn.Tri.Naga@gmail.com>
2017-10-23 18:14:55 +0900

    added expression breaking rule

--- src/expression_parser.rs
+++ src/expression_parser.rs
@@ -43,9 +43,9 @@ impl<'s> Deref for Expression<'s>
 /// ```
 /// # use pureshader::*;
 /// # use std::cell::RefCell;
-/// let (s, v) = (RefCell::new(Source::new("23 + ft (vec2 4 0).x")), RefCell::new(Vec::new()));
+/// let (s, v) = (RefCell::new(Source::new("23 + ft (vec2 4 0).x\n4")), RefCell::new(Vec::new()));
 /// let mut tokcache = TokenizerCache::new(&v, &s);
-/// let expr = expression(&mut tokcache, None).unwrap();
+/// let expr = expression(&mut tokcache, None, None).unwrap();
 /// assert_eq!(expr[0].text(), Some("23"));
 /// assert_eq!(expr[1].text(), Some("+"));
 /// assert_eq!(expr[2].text(), Some("ft"));
@@ -54,18 +54,25 @@ impl<'s> Deref for Expression<'s>
 /// assert_eq!(expr[3].children().unwrap()[2].text(), Some("0"));
 /// assert_eq!(expr[4], ExpressionFragment::ObjectDescender(Location { line: 1, column: 19 }));
 /// assert_eq!(expr[5].text(), Some("x"));
+/// assert_eq!(tokcache.current(), &Token::Numeric(Source { pos: Location { line: 2, column: 1 }, slice: "4" }, None));
 /// ```
-pub fn expression<'s: 't, 't>(stream: &mut TokenizerCache<'s, 't>, correspond_closing: Option<EnclosureKind>) -> Result<Expression<'s>, ParseError<'t>>
+pub fn expression<'s: 't, 't>(stream: &mut TokenizerCache<'s, 't>, expression_base: Option<usize>, correspond_closing: Option<EnclosureKind>) -> Result<Expression<'s>, ParseError<'t>>
 {
     let mut v = Vec::new();
+    let mut last_line = stream.current().position().line;
+    let expression_base = expression_base.unwrap_or_else(|| stream.current().position().column);
     loop
     {
-        match *stream.next()
+        let t = stream.next();
+        // break expression on less/equal indentation
+        if t.position().line != last_line && t.position().column <= expression_base { stream.unshift(); break; }
+        last_line = t.position().line;
+        match *t
         {
             Token::EOF(ref p) => if let Some(k) = correspond_closing { return Err(ParseError::ExpectingClose(k, p)); } else { break; },
             Token::BeginEnclosure(ref p, k@EnclosureKind::Parenthese) | Token::BeginEnclosure(ref p, k@EnclosureKind::Brace) =>
             {
-                v.place_back() <- ExpressionFragment::Primary(p.clone(), box expression(stream, Some(k))?);
+                v.place_back() <- ExpressionFragment::Primary(p.clone(), box expression(stream, Some(expression_base), Some(k))?);
             },
             Token::EndEnclosure(_, k) if Some(k) == correspond_closing => break,
             Token::EndEnclosure(ref p, k) => { stream.unshift(); return Err(ParseError::UnexpectedClose(k, p)); },
