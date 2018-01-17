58adc679a33932901e40bacc650721576821dc38
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-01-17 13:13:04 +0900

    fixed custom derive

--- pshcompile-custom-derive/src/lib.rs
+++ pshcompile-custom-derive/src/lib.rs
@@ -5,22 +5,23 @@ extern crate syn;
 #[macro_use] extern crate quote;
 
 use proc_macro::TokenStream;
+use syn::{MetaItem, Lit};
 
 #[proc_macro_derive(SymbolScope, attributes(SymbolMapReferer))]
 pub fn impl_symbol_scope(input: TokenStream) -> TokenStream
 {
-    let ast: DeriveInput = syn::parse(input).unwrap();
-    let ref tyname = ast.ident;
-    let referer = ast.attrs.iter().filter_map(|a| match a.interpret_meta()
+    let s = input.to_string();
+    let ast = syn::parse_derive_input(&s).unwrap();
+    let referer = ast.attrs.iter().filter_map(|a| match a.value
     {
-        Some(syn::Meta::NameValue(syn::MetaNameValue { ident: "SymbolMapReferer", ref lit, .. })) => match *lit
+        MetaItem::NameValue(ref ident, ref lit) if ident == "SymbolMapReferer" => match *lit
         {
-            syn::Lit::Str(ref l) => Some(l.value()),
-            syn::Lit::Int(ref n) => Some(n.value().to_string()),
+            Lit::Str(ref l, _) => Some(l.to_owned()), Lit::Int(n, _) => Some(n.to_string()),
             _ => panic!("`SymbolMapReferer` requires a string literal or an integer literal")
         }
         _ => None
     }).next().unwrap_or("0".to_string());
+    let ref tyname = ast.ident;
     let gen = quote!{
         impl<'s: 't, 't> ::symbol::SymbolScope<'s, 't> for #tyname<'s, 't>
         {
