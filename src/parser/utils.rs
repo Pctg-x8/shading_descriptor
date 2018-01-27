c2db2d085e086e32284481e853eb26a3a0f628d7
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-01-27 14:00:27 +0900

    added inclusive/exclusive nothing for Leftmost

--- src/parser/utils.rs
+++ src/parser/utils.rs
@@ -1,17 +1,17 @@
 //! Parser utils
 
-use tokparse::{TokenStream, TokenKind, EnclosureKind, Source, Location};
+use {TokenStream, TokenKind, EnclosureKind, Source, Location, Keyword};
 use parser::err::*;
 
 macro_rules! CheckLayout
 {
 	($leftmost: expr => $stream: expr) =>
 	{
-		if !$leftmost.satisfy($stream.current(), true) { return Err(ParseError::LayoutViolation($stream.current().position())).into(); }
+		if !$leftmost.satisfy($stream.current()) { return Err(ParseError::LayoutViolation($stream.current().position())).into(); }
 	};
 	($leftmost: expr => $stream: expr, NC) =>
 	{
-		if !$leftmost.satisfy($stream.current(), true) { return NotConsumed; }
+		if !$leftmost.satisfy($stream.current()) { return NotConsumed; }
 	}
 }
 /// トークンマッチングマクロ
@@ -92,28 +92,23 @@ macro_rules! BreakParsing
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Leftmost { Nothing, Inclusive(usize), Exclusive(usize) }
+pub enum Leftmost { NothingInc, NothingEx, Inclusive(usize), Exclusive(usize) }
 impl Leftmost
 {
-	pub fn satisfy(&self, tok: &TokenKind, always_satisfy_on_nothing: bool) -> bool
+	pub fn satisfy(&self, tok: &TokenKind) -> bool
 	{
 		match *self
 		{
 			Leftmost::Inclusive(l) => l <= tok.position().column,
 			Leftmost::Exclusive(l) => l < tok.position().column,
-			Leftmost::Nothing => always_satisfy_on_nothing
+			Leftmost::NothingInc => true, Leftmost::NothingEx => false
 		}
 	}
-	pub fn num(&self) -> Option<usize> { match *self { Leftmost::Nothing => None, Leftmost::Inclusive(n) | Leftmost::Exclusive(n) => Some(n) } }
-	pub fn is_nothing(&self) -> bool { match *self { Leftmost::Nothing => true, _ => false } }
-	pub fn into_exclusive(self) -> Self
-	{
-		match self { Leftmost::Nothing | Leftmost::Exclusive(_) => self, Leftmost::Inclusive(n) => Leftmost::Exclusive(n) }
-	}
-	pub fn into_nothing_as(self, nothing: Leftmost) -> Self
-	{
-		match self { Leftmost::Nothing => nothing, _ => self }
-	}
+	pub fn num(&self) -> Option<usize> { match *self { Leftmost::NothingInc | Leftmost::NothingEx => None, Leftmost::Inclusive(n) | Leftmost::Exclusive(n) => Some(n) } }
+	pub fn is_nothing(&self) -> bool { match *self { Leftmost::NothingInc | Leftmost::NothingEx => true, _ => false } }
+	pub fn into_nothing_as(self, nothing: Leftmost) -> Self { match self { Leftmost::NothingInc | Leftmost::NothingEx => nothing, _ => self } }
+	pub fn into_inclusive(self) -> Self { match self { Leftmost::NothingEx => Leftmost::NothingInc, Leftmost::Exclusive(n) => Leftmost::Inclusive(n), t => t } }
+	pub fn into_exclusive(self) -> Self { match self { Leftmost::NothingInc => Leftmost::NothingEx, Leftmost::Inclusive(n) => Leftmost::Exclusive(n), t => t } }
 	pub fn is_explicit(&self) -> bool { self.is_nothing() }
 }
 
@@ -150,7 +145,7 @@ pub fn name<'s: 't, 't, S: TokenStream<'s, 't>>(stream: &mut S, leftmost: Leftmo
 /// op | infixindent
 pub fn shift_infix_ops<'s: 't, 't, S: TokenStream<'s, 't>>(stream: &mut S, leftmost: Leftmost) -> Result<&'t Source<'s>, &'t Location>
 {
-	if !leftmost.satisfy(stream.current(), true) { return Err(stream.current().position()); }
+	if !leftmost.into_inclusive().satisfy(stream.current()) { return Err(stream.current().position()); }
     match stream.current()
     {
         &TokenKind::Operator(ref s) | &TokenKind::InfixIdent(ref s) => { stream.shift(); Ok(s) },
@@ -160,16 +155,25 @@ pub fn shift_infix_ops<'s: 't, 't, S: TokenStream<'s, 't>>(stream: &mut S, leftm
 /// ident | wrapped_op
 pub fn shift_prefix_declarator<'s: 't, 't, S: TokenStream<'s, 't>>(stream: &mut S, leftmost: Leftmost) -> ParseResult<'t, &'t Source<'s>>
 {
-	let leftmost = leftmost.into_nothing_as(Leftmost::Inclusive(stream.current().position().column));
-	if !leftmost.satisfy(stream.current(), true) { return NotConsumed; }
+	let leftmost = leftmost.into_nothing_as(Leftmost::Inclusive(stream.current().position().column)).into_inclusive();
+	if !leftmost.satisfy(stream.current()) { return NotConsumed; }
 	match *stream.current()
 	{
 		TokenKind::Identifier(ref s) | TokenKind::WrappedOp(ref s) => { stream.shift(); Success(s) },
 		_ => NotConsumed
 	}
 }
-pub fn shift_satisfy_leftmost<'s: 't, 't, S, F, T>(stream: &mut S, leftmost: Leftmost, satisfy_on_nothing: bool, shifter: F) -> Result<T, &'t Location>
+/// `where` | `:`
+pub fn shift_block_begin<'s: 't, 't, S: TokenStream<'s, 't>>(stream: &mut S, leftmost: Leftmost) -> Result<&'t Location, &'t Location>
+{
+	shift_satisfy_leftmost(stream, leftmost, |s| match *s.current()
+	{
+		TokenKind::Keyword(ref p, Keyword::Where) | TokenKind::ItemDescriptorDelimiter(ref p) => { s.shift(); Ok(p) },
+		t => Err(t.position())
+	})
+}
+pub fn shift_satisfy_leftmost<'s: 't, 't, S, F, T>(stream: &mut S, leftmost: Leftmost, shifter: F) -> Result<T, &'t Location>
     where S: TokenStream<'s, 't>, F: FnOnce(&mut S) -> Result<T, &'t Location>
 {
-    if leftmost.satisfy(stream.current(), satisfy_on_nothing) { shifter(stream) } else { Err(stream.current().position()) }
+    if leftmost.satisfy(stream.current()) { shifter(stream) } else { Err(stream.current().position()) }
 }
